2a76ad23acd36c8616b7e348aed2cc15ef9640b3
john L <johnleavell@gmail.com>
2019-09-01 02:24:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var name = "Susan"
+var name = "Bob"
 var height = 70
 
 if (name === "Susan") {
   console.log("The name is correct")
+} else{
+  console.log("Expected the name to be Susan")
 }
 
 
